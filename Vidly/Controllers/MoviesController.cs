1a563c33adbd8fab4cb012bd6012167624125fd8
Bimsara Bodaragama <bodaragamadb.office@gmail.com>
2022-10-24 14:53:18 +0530

    Working with Data - Part 3 - Modifications Movies List & Details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Ajax.Utilities;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -11,22 +9,35 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
-
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
-
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>{
-                new Movie{Id=1, Name="Shrek"},
-                new Movie{Id=2, Name="Wall-e"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
         }
+
 
         // GET: Movies/Random
         public ActionResult Random()
@@ -34,48 +45,15 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer{Name = "Customer 1"},
-                new Customer{Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-
-            var veiwModel = new RandomMovieViewModel()
+            var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
-
-            return View(veiwModel);
+            return View(viewModel);
         }
-
-        /*
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        */
-
     }
 }
